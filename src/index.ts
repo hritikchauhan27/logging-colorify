0bbd66c366c30904b8fb9e9af7168db877edce41
hritikchauhan27 <hritik.chauhan@appinventiv.com>
2023-09-06 17:09:19 +0530

    upgraded commit using ASNI escape code

--- src/index.ts
+++ src/index.ts
@@ -7,42 +7,39 @@ export class Log {
         const bgColorCode = bgColors[bgColor || 'black'];
 
         const timestamp = new Date().toLocaleString();
+        const logMessage = `[${timestamp}] ${msg}`;
 
         if (colorCode && bgColorCode) {
-            console.log(`\x1b[${bgColorCode};${colorCode}m[${timestamp}] ${msg}\x1b[0m`);
-            return `[${timestamp}] ${msg}`;
+            console.log(`\x1b[${bgColorCode};${colorCode}m${logMessage}\x1b[0m`);
         } else if (colorCode) {
-            console.log(`\x1b[${colorCode}m[${timestamp}] ${msg}\x1b[0m`);
-            return `[${timestamp}] ${msg}`;
+            console.log(`\x1b[${colorCode}m${logMessage}\x1b[0m`);
         } else {
-            console.log(`[${timestamp}] ${msg}`);
-            return `[${timestamp}] ${msg}`;
+            console.log(logMessage);
         }
 
-
+        return logMessage;
     }
 
-    private static writeToLogFile(msg: string, filename: string) {
-        fs.appendFile(filename, msg + '\n', (err) => {
-            if (err) {
-                console.error('Error writing to log file:', err);
-            }
-        });
+    private static async writeToLogFile(msg: string, filename: string) {
+        try {
+            await fs.promises.appendFile(filename, msg + '\n');
+        } catch (err) {
+            console.error('Error writing to log file:', err);
+        }
     }
 
-    static error(msg: string) {
+    static async error(msg: string) {
         const logMessage = this.logWithColor(msg, 'red', 'white');
-        this.writeToLogFile(logMessage, 'error.log');
+        await this.writeToLogFile(logMessage, 'error.log');
     }
 
-    static info(msg: string) {
+    static async info(msg: string) {
         const logMessage = this.logWithColor(msg, 'blue', 'white');
-        this.writeToLogFile(logMessage, 'info.log');
+        await this.writeToLogFile(logMessage, 'info.log');
     }
 
-    static warn(msg: string) {
+    static async warn(msg: string) {
         const logMessage = this.logWithColor(msg, 'yellow', 'black');
-        this.writeToLogFile(logMessage, 'warn.log');
+        await this.writeToLogFile(logMessage, 'warn.log');
     }
-
 }
