70f2d86aaf0117bb350e166e5fc21c7eb64b86e9
hritikchauhan27 <hritik.chauhan@appinventiv.com>
2023-09-06 14:26:53 +0530

    upgraded commit using ASNI escape code

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,38 @@
-import chalk = require("chalk");
-
-export class Log{
-    static info(msg: string){
-        console.log(chalk.blue.bgYellow(msg));
+export class Log {
+    static logWithColor(msg: string, color: string, bgColor?: string) {
+      const colors: { [key: string]: string } = {
+        black: '30',
+        red: '31',
+        green: '32',
+        yellow: '33',
+        blue: '34',
+        magenta: '35',
+        cyan: '36',
+        white: '37',
+      };
+  
+      const bgColors: { [key: string]: string } = {
+        black: '40',
+        red: '41',
+        green: '42',
+        yellow: '43',
+        blue: '44',
+        magenta: '45',
+        cyan: '46',
+        white: '47',
+      };
+  
+      const colorCode = colors[color];
+      const bgColorCode = bgColors[bgColor || 'black'];
+  
+      const timestamp = new Date().toLocaleString();
+  
+      if (colorCode && bgColorCode) {
+        console.log(`\x1b[${bgColorCode};${colorCode}m[${timestamp}] ${msg}\x1b[0m`); //----  \x1b[36m%s\x1b[0m
+      } else if (colorCode) {
+        console.log(`\x1b[${colorCode}m[${timestamp}] ${msg}\x1b[0m`);
+      } else {
+        console.log(`[${timestamp}] ${msg}`);
+      }
     }
-
-    static warn(msg:string){
-        console.log(chalk.yellow(msg));
-    }
-
-    static error(msg:string){
-        console.log(chalk.red(msg));
-    }
-    
-}
+  }
